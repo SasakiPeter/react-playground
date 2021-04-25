f66abdccc029c1b9c15b1d5277dd4ed519660828
SasakiPeter <36841326+SasakiPeter@users.noreply.github.com>
2021-04-26 01:38:53 +0900

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  plugins: ["@typescript-eslint"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "prettier",
+  ],
+  rules: {
+    "react/prop-types": "off",
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off",
+  },
+};
---
+++ webpack.config.ts
@@ -0,0 +1,75 @@
+import type { Configuration } from "webpack";
+import type { Configuration as DevConfiguration } from "webpack-dev-server";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import { resolve } from "path";
+
+const isProduction = process.env.NODE_ENV === "production";
+
+declare module "webpack" {
+  interface Configuration {
+    devServer?: DevConfiguration;
+  }
+}
+
+const config: Configuration = {
+  target: "web",
+  mode: isProduction ? "production" : "development",
+  entry: {
+    index: resolve(__dirname, "src", "index.tsx"),
+  },
+  output: {
+    path: resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    historyApiFallback: true,
+    port: 3000,
+    hot: true,
+    open: true,
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: "babel-loader" },
+          {
+            loader: "ts-loader",
+            options: {
+              configFile: resolve(__dirname, "src", "tsconfig.json"),
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: {
+                auto: true,
+                localIdentName: isProduction
+                  ? "[hash:base64:16]"
+                  : "[path][name]_[local]",
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      minify: isProduction,
+      template: resolve(__dirname, "src", "index.html"),
+    }),
+  ],
+};
+
+export default config;
